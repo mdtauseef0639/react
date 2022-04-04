0fde2900e1bb24cf921859f09a1e4321eae6178a
Md Tauseef Kamal <t.kamal@axelor.com>
2022-04-04 17:11:30 +0530

    mixed month and year

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,27 +1,66 @@
 import React, { useState } from 'react'
-import Days from './Days';
 
 export default function App() {
     
-    // d.setDate(1);
+    const d = new Date()
+   
+    
     const months = [ "January" , "February" , "March" , "April" , "May" , "June" , "July" , "August" , "September" , "October" , "November" , "December"];
-    const [date,setDate] = useState(new Date()); 
+    
+    const [date,setDate] = useState({
+        month:d.getMonth(),
+        year:d.getFullYear()
+    }) 
 
-    const lastDay =  new Date(date.getFullYear(),date.getMonth()+1,0).getDate();
-    const days = [];
-    for(let i=1;i<=lastDay;i++)
-    {
-        days.push(i)
-    }
+    const lastDay =  new Date(date.year,date.month+1,0).getDate();
+
+   const days = []
+
+   for(let i=1;i<lastDay;i++)
+   {
+       days.push(i)
+   }
+   console.log(days)
+    
     function handleLeft()
     {
-        setDate(date.setMonth()-1)
+        setDate(prevValue=>{
+            if(prevValue.month===0)
+            {
+                return{
+                    month:11,
+                    year:prevValue.year-1
+                }
+            }
+            else
+            {
+                return{
+                    month:prevValue.month-1,
+                    year:prevValue.year
+                }
+            }
+        })
         
     }
 
     function handleRight()
     {
-        setDate(date.setMonth()+1)
+        setDate(prevValue=>{
+            if(prevValue.month===11)
+            {
+                return{
+                    month:0,
+                    year:prevValue.year+1
+                }
+            }
+            else
+            {
+                return{
+                    month:prevValue.month+1,
+                    year:prevValue.year
+                }
+            }
+        })
 
     }
 
@@ -31,11 +70,10 @@ export default function App() {
     <div className="container">
         <div className="calendar">
             <div className="month">
-                {/* <i className="fas fa-angle-left prev" onClick={handleLeft}></i> */}
-                <button onClick={handleLeft}>{"<"}</button>
+                <i className="fas fa-angle-left prev" onClick={handleLeft}></i>
                 <div className="date">
-                    <h1>{months[date.getMonth()]}</h1>
-                    <p>{date.toDateString()}</p>
+                    <h1>{months[date.month]}</h1>
+                    <p>{new Date(date.year,date.month,d.getDate()).toDateString()}</p>
                 </div>
                 <i className="fas fa-angle-right next" onClick={handleRight}></i>
             </div>
@@ -48,10 +86,42 @@ export default function App() {
                 <div>Fri</div>
                 <div>Sat</div>
             </div>
-            <div className="days">
-        <Days date ={date}days={days}/>
-    </div>
+            {/* <div className="days">
+            {
+                days.map((i)=>{
+                    if(new Date(date.getFullYear(),date.getMonth(),i).getDay()==0 || new Date(date.getFullYear(),date.getMonth(),i).getDay()==6)
+                {
+                    if(i===new Date().getDate() && date.getMonth() === new Date().getMonth())
+                {
+                    
+                    <div class="today weekends">${i}</div>;
+                    
+                }
+                else{
+                    <div class="weekends">${i}</div>
+                
+                }
+                }
+                else{
+                    if(i===new Date().getDate() && date.getMonth() === new Date().getMonth())
+                {
+                    
+                       <div class="today">${i}</div>
+            
+                }
+                else{
+                <div>${i}</div>
+                }
+                }
+                  })
+              }
+            
+    </div> */}
         </div>
     </div>
   )
+
+
+
+  
 }
